d3e81bb983e8bbf4e1cb9ec174b995e3f0b9e72a
Elderiano <108804866+Elderiano@users.noreply.github.com>
2024-06-10 11:23:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        Alternativas: [
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+
+    {
+        enunciado: "Pergunta 2",
+        Alternativas: [
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+]
+
+let atual = 0
+let perguntaAtual
+
+function mostrarPergunta() {
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+}
+
+mostrarPergunta()
